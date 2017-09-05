77efa2d744076cfcdaf22e6a6f387305144a3c53
Prakashbehara <prakashbehara@gmail.com>
2017-09-04 23:13:22 -0400

    Chnages done to render the Datatable UI
    Chnages done to render the Datatable UI dynamiccaly with JSON response.

--- RMTSAppBuilder/UIApp/src/components/DataTable.tsx
+++ RMTSAppBuilder/UIApp/src/components/DataTable.tsx
@@ -2,8 +2,7 @@ import * as React from 'react';
 import {
   Table,
   TableBody,
-  TableHeader,
-  TableHeaderColumn,
+  TableHeader,  
   TableRow,
   TableRowColumn,
 } from 'material-ui/Table'; 
@@ -12,75 +11,60 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 export interface TableProp{
  inputTableData : Array<any>;
- 
+ headerData : Array<any>;
  gridHandler : any;
  addDataHandler : any;
  modifyDataHandler : any;
  deleteDataHandler : any;
-
  modifyButtonStatus : boolean;
  deleteButtonStatus : boolean;
-
 }
-
 export interface TableState{
   open : boolean;
 }
-
-
-
 export class DataTable extends React.Component<TableProp, TableState> {
   props : TableProp; 
   state : TableState ;
   constructor(props : TableProp){
     super(props); 
   }
-
   render() {
     var style = {
              margin: 12            
     };
-
-
     return (
     <Paper zDepth={2} >
     <RaisedButton label="Add" primary={true} style={style} onClick={this.props.addDataHandler}   />
     <RaisedButton label="Modify" secondary={true} onClick = {this.props.modifyDataHandler} disabled = {this.props.modifyButtonStatus} />
     <RaisedButton label="Delete"  style={style} onClick = {this.props.deleteDataHandler}  
          disabled = {this.props.deleteButtonStatus}/>
-
     <Table multiSelectable={true} onRowSelection={this.props.gridHandler} >
     <TableHeader>
-      <TableRow>
-        <TableHeaderColumn>ID</TableHeaderColumn>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-        <TableHeaderColumn>Status</TableHeaderColumn>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-        <TableHeaderColumn>Status</TableHeaderColumn>
-        <TableHeaderColumn>Name</TableHeaderColumn>
-        <TableHeaderColumn>Status</TableHeaderColumn>
+      <TableRow> 
+       {this.props.headerData}
       </TableRow>
     </TableHeader>
-    <TableBody deselectOnClickaway = {false} displayRowCheckbox = {true} preScanRows={false}>
-      {this.props.inputTableData.map( (row, index) => (
-              <TableRow key={index} selected = {row.selected} >
-                <TableRowColumn>{index}</TableRowColumn>
-                <TableRowColumn>{row.name}</TableRowColumn>
-                <TableRowColumn>{row.status}</TableRowColumn>
-                <TableRowColumn>{row.name}</TableRowColumn>
-                <TableRowColumn>{row.status}</TableRowColumn>
-                <TableRowColumn>{row.name}</TableRowColumn>
-                <TableRowColumn>{row.status}</TableRowColumn>
+    <TableBody deselectOnClickaway = {false} displayRowCheckbox = {true} preScanRows={true}>
+      {
+          this.props.inputTableData.map( (p : any) => {
+            var items = Object.keys(p)     
+            .filter(k => k !== 'selected' )
+            .map(k => {
+              return (                
+                  <TableRowColumn key={p.id + k}>{p[k]}</TableRowColumn>
+              )
+            });
+            return (
+              <TableRow key={p.id} selected={p.selected == 1} >
+                {items}
               </TableRow>
-              ))}
-
+            );
+          })  
+      }
     </TableBody>
   </Table>
-
-  </Paper>
- 
+  </Paper> 
     );
   }
 }
-
 export default DataTable;
