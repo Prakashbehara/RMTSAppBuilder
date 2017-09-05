6a43fcf1d717c3d711ea9ae0ba1b8473e19822ee
Prakashbehara <prakashbehara@gmail.com>
2017-09-04 23:15:32 -0400

    Chnages done to render the Datatable UI
    Chnages done to render the Datatable UI dynamiccaly with JSON response.

--- RMTSAppBuilder/UIApp/src/components/DataTableFilter.tsx
+++ RMTSAppBuilder/UIApp/src/components/DataTableFilter.tsx
@@ -2,6 +2,14 @@ import * as React from 'react';
  
 import {DataTable} from './DataTable';
 import {DataTableHeader} from './DataTableHeader';
+import {AddDataDialog} from './AddDataDialog';
+import TextField from 'material-ui/TextField';
+
+import { 
+  TableHeaderColumn
+} from 'material-ui/Table'; 
+var Client = require('node-rest-client').Client;
+var client = new Client();
 
 export interface TableFilterProp{
  inputTableData : Array<any>; 
@@ -9,27 +17,54 @@ export interface TableFilterProp{
 
 export interface TableFilterState{
   tableData : Array<any>;
+  headerData : Array<any>;
   deleteButton : boolean;
   modifyButton : boolean;
   selectedRows : Array<number>; 
+  openAddDialog : boolean;
+  dialogComponents : Array<any>;
 }
 
 class DataTableFilter extends React.Component<TableFilterProp, TableFilterState> {
   props : TableFilterProp; 
   state : TableFilterState = {tableData : this.props.inputTableData , 
-     deleteButton : true , modifyButton : true , selectedRows : []};
-  
+     deleteButton : true , modifyButton : true , selectedRows : [] , headerData : [], 
+     openAddDialog : false, dialogComponents : []};  
   constructor(props : TableFilterProp){
     super(props); 
     this.dataTableHandler = this.dataTableHandler.bind(this);
     this.addData = this.addData.bind(this);
     this.modifyData = this.modifyData.bind(this);
     this.deleteData = this.deleteData.bind(this);
+    this.parseData = this.parseData.bind(this);
+    this.closeAddDataDialog = this.closeAddDataDialog.bind(this);
+    this.onBlurValue = this.onBlurValue.bind(this);
   }
 
+  parseData(data1 :any , response : any) {
+     // parsed response body as js object
+     var headerData1= Object.keys(data1[0]).splice(1).map((element, index, array)=> {           
+            return <TableHeaderColumn> {element} </TableHeaderColumn>;
+     });
+     this.setState ( {tableData: data1 , headerData : headerData1});
+  };
+
+  componentDidMount() {    
+    // registering remote methods 
+    client.registerMethod("jsonMethod", "http://localhost:3001/tasks", "GET");
+    var req = client.methods.jsonMethod(this.parseData);
+        req.on('error', function (err: any ) {
+          alert('error ');
+            console.log('request error', err);
+        });
+  }
+
+onBlurValue(event: object){
+    alert('onBlurValue' + event);
+}
 dataTableHandler(rowNumber : any  ) { 
         if(rowNumber == 'all'){
-            this.setState( {modifyButton : false , deleteButton : false} );      
+            this.setState( {modifyButton : true , deleteButton : true} );      
         }else if(rowNumber == 'none'){
             this.setState( {modifyButton : true , deleteButton : true} );
         }else{
@@ -47,10 +82,10 @@ dataTableHandler(rowNumber : any  ) {
                     this.setState( { modifyButton : true} );
                 } 
                 this.state.tableData.map( (row, index) => {
-                    row.selected = false;
+                    row.selected = 0;
                 });
                 rowNumber.forEach(index => {
-                    this.state.tableData[index].selected = true;         
+                    this.state.tableData[index].selected = 1;         
                 });
             }
             this.setState({ tableData: this.state.tableData } );          
@@ -58,9 +93,17 @@ dataTableHandler(rowNumber : any  ) {
 }
 
 addData (){
-    var data = this.state.tableData;
-    data.push({name: 'Raja', status: 'Babu'});
-    this.setState({tableData : data});
+    var dialogComponents = Object.keys(this.state.tableData[0]).splice(1).map((element, index, array)=> {           
+            return <tr><td><TextField id={element} hintText={element} onBlur={this.onBlurValue}  ></TextField></td></tr>;
+     });
+     this.setState({openAddDialog : true , dialogComponents : dialogComponents});
+    /*var data = this.state.tableData;
+    data.push({Name: 'Raja', City: 'Westborough', State : 'MA'});
+    this.setState({tableData : data});*/
+}
+
+closeAddDataDialog(){
+    this.setState({openAddDialog : false});
 }
 
 modifyData (){
@@ -81,9 +124,13 @@ deleteData (){
             <br />
             <DataTable 
                 inputTableData={this.state.tableData}  
+                headerData = {this.state.headerData}
                 gridHandler = {this.dataTableHandler} addDataHandler = {this.addData} deleteDataHandler = {this.deleteData}
                 modifyDataHandler = {this.modifyData} deleteButtonStatus = {this.state.deleteButton} 
                 modifyButtonStatus = {this.state.modifyButton}/>
+            <AddDataDialog open={this.state.openAddDialog} 
+                formData = {this.state.dialogComponents}
+                handler={this.closeAddDataDialog}></AddDataDialog>
         </div>
     );
   }
